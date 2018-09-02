d7faa6f3a503a6b288d2536b02de56f3d675bc30
Suheyl Turac <skbsturac@live.com>
2018-09-02 06:32:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,43 +10,84 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- var itemPrice = Math.floor(Math.random()*100)
- var newItem = {}
-  newItem[itemName]=itemPrice
-cart.push(newItem)
-return (`${itemName} has been added to your cart.`)
+  var item = generateCartItem(item)
+  getCart().push(item)
+  return `${item.itemName} has been added to your cart.`
 }
 
 function viewCart() {
- let i = 0
-  var printedArr =[]
-  if(cart.length===0){
-    return(`Your shopping cart is empty.`)
-  }
+  return getCart().length === 0 ? "Your shopping cart is empty." : generateCartDescription()
 }
 
 function total() {
-  // write your code here
+  var sum = sumUpPrices()
+  return sum
 }
 
-function removeFromCart(item) {
-  let i = 0
-  var printedArr =[]
-  if(cart.length===0){
-    return(`Your shopping cart is empty.`)
-  }
-  else if(cart.length< 3){
-    for (let i= 0; i<cart.length; i++){
-   printedArr.push(`${Object.keys(cart[i])} at $${Object.values(cart[i])}`)
-  }
-  return (`In your cart, you have ${printedArr.join(" and ")}.`)
-  }else{
-    for (let i= 0; i<cart.length-1; i++){
-      printedArr.push(`${Object.keys(cart[i])} at $${Object.values(cart[i])}`)
-    }
-    return(`In your cart, you have ${printedArr.join(", ")}, and ${Object.keys(cart[cart.length-1])} at $${Object.values(cart[cart.length-1])}.`)
+function removeFromCart(itemName) {
+  var itemToRemove = searchCartForItemToRemove(itemName)
+  return itemToRemove ? removeItemFromCart(itemToRemove) : notifyUserThereIsNoItemToRemove()
+}
+
+function placeOrder(cardNumber) {
+  if (arguments[0] === undefined) {
+    return "Sorry, we don't have a credit card on file for you."
+  } else {
+    var sumToCharge = total()
+    setCart([])
+    return `Your total cost is $${sumToCharge}, which will be charged to the card ${cardNumber}.`
   }
 }
-function placeOrder(cardNumber) {
-  // write your code here
+
+// helper functions
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function generateCartItem(itemName) {
+  return {
+    itemName:itemName,
+    itemPrice:getRandomInt(1, 100)
+  }
+}
+
+function generateCartDescription() {
+  var cartDescription = 'In your cart, you have '
+  if ( getCart().length >= 1 ) {
+    cartDescription += `${getCart()[0].itemName} at $${getCart()[0].itemPrice}`
+  }
+  if ( getCart().length >= 2 ) {
+    var middleCartItemsDescription = ''
+    for (var i=1; i<getCart().length -1; i++) {
+      middleCartItemsDescription += `, ${getCart()[i].itemName} at $${getCart()[i].itemPrice}`
+    }
+    cartDescription += `${middleCartItemsDescription}, and ${getCart()[getCart().length-1].itemName} at $${getCart()[getCart().length-1].itemPrice}`
+  }
+
+  return `${cartDescription}.`
+}
+
+function searchCartForItemToRemove(itemName) {
+  var searchResult
+  for (var i=0; i<getCart().length; i++) {
+    if (getCart()[i].itemName === itemName) {searchResult = getCart()[i]}
+  }
+  return searchResult
+}
+
+function sumUpPrices() {
+  var sum = 0
+  for (var i=0; i<getCart().length; i++) {
+    sum = sum + getCart()[i].itemPrice
+  }
+  return sum
+}
+
+function notifyUserThereIsNoItemToRemove() {
+  return 'That item is not in your cart.'
+}
+
+function removeItemFromCart(itemToRemove) {
+  var indexOfItemToRemove = cart.indexOf(itemToRemove)
+  getCart().splice(indexOfItemToRemove,1)
 }
